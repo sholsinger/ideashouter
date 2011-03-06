f4b9907dafe27733325b213b8fb2662335dc18fd
Stephen Holsinger <sholsinger@gmail.com>
2011-03-05 19:26:26 -0500

    initial commit. This repo has a bunch of extra scaffolding which was auto-generated for me cause I got stuck without a build system and spent half the meetup installing XCode and building ruby.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ideashouter::Application.config.secret_token = 'c3ca96482e42893463f43e70d291767d33d53d5f98975d3f8f8c970f752abd11515253edd591917f6df5300deac9cf46e97f940741b5f92758a32446a8a5cab9'
